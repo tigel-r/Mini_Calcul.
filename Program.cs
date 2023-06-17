15e5015bd4eef7726141fc0b25a17530bf7cf756
tigel-r <tigel-r@mail.ru>
2023-06-17 18:34:27 +0300

    creating logic

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.Clear();
+    {
+        double FirstVal, SecondVal;
+        string Action;
+
+        
+        FirstVal = double.Parse(Console.ReadLine());
+
+        
+        SecondVal = double.Parse(Console.ReadLine());
+
+        
+        Action = Console.ReadLine();
+
+        switch (Action)
+        {
+            case "+":
+                Console.WriteLine(FirstVal + SecondVal);
+                break;
+            case "-":
+                Console.WriteLine(FirstVal - SecondVal);
+                break;
+            case "*":
+                Console.WriteLine(FirstVal * SecondVal);
+                break;
+            case "/":
+                Console.WriteLine(FirstVal / SecondVal);
+                break;
+            default:
+                
+                break;
+
+        }
+
+        Console.ReadLine();
+
+    }
+
+
+
+}
+
